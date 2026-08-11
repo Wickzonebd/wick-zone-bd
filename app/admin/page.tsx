7c33a9859a9501cb53d3c73466b3aff796293f9d
Wickzonebd <iamthegod973@gmail.com>
2026-08-11 10:47:58 +0600

    Expose payment control center from admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,2 +1,4 @@
 import { AdminClient } from "@/components/admin-client";
-export default function AdminPage() { return <AdminClient />; }
+import { AdminPaymentShortcut } from "@/components/admin-payment-shortcut";
+
+export default function AdminPage() { return <><AdminClient /><AdminPaymentShortcut /></>; }
