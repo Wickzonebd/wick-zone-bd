3e2b71dafb7bc56bbd67f1ae47ff0a1a3239af94
Wickzonebd <iamthegod973@gmail.com>
2026-08-20 10:21:55 +0600

    Mount login notice globally

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,13 +2,17 @@
 
 import { AuthProvider } from "@/components/auth-provider";
 import { I18nProvider } from "@/components/i18n-provider";
+import { LoginNotice } from "@/components/login-notice";
 import { SiteConfigProvider } from "@/components/site-config-provider";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <I18nProvider>
       <SiteConfigProvider>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          {children}
+          <LoginNotice />
+        </AuthProvider>
       </SiteConfigProvider>
     </I18nProvider>
   );
